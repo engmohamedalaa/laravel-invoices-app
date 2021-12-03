02911af7277f8acbdb3b61ae2ed8223749aec7f4
Mohamed Alaa <medo2012421@gmail.com>
2021-12-04 00:33:54 +0200

    make home stats

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
 	<div class="main-footer ht-40">
 		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>Copyright © 2020 <a href="#">Valex</a>. Designed by <a href="https://www.spruko.com/">Spruko</a> All rights reserved.</span>
+			<span>Copyright © 2021 <a href="#">Invoices System</a>. Developed by <a href="https://www.egy2d.com/">EGY2D</a> All rights reserved.</span>
 		</div>
 	</div>
 <!-- Footer closed -->
