082a8c070088b81c6457cb2a3488ff85b673c34d
Mohamed Alaa <medo2012421@gmail.com>
2021-12-04 17:09:33 +0200

    add some edits

--- public/index.php
+++ public/index.php
@@ -22,6 +22,7 @@ define('LARAVEL_START', microtime(true));
 */
 
 require __DIR__.'/../vendor/autoload.php';
+//require __DIR__.'/../laravel/vendor/autoload.php'; //at server to organize the project
 
 /*
 |--------------------------------------------------------------------------
