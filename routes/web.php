397ac27183efb88f50fa3daf15692857294e15fd
Mohamed Alaa <medo2012421@gmail.com>
2021-12-05 12:54:28 -0700

    make some edits on server

--- routes/web.php
+++ routes/web.php
@@ -13,53 +13,53 @@ use App\Http\Controllers\PhoneAuthController;
 |
 */
 
-Route::get('/', function () {
+Route::get('//', function () {
     return view('auth.login'); //home page -> index
 });
 
 Auth::routes();
 //Auth::routes(['register' => false]);
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('//home', 'HomeController@index')->name('home');
 Route::middleware('auth')->group(function () {
-    Route::resource('invoices', 'InvoicesController');
-    Route::resource('archive', 'InvoiceAchiveController');
+    Route::resource('/invoices', 'InvoicesController');
+    Route::resource('/archive', 'InvoiceAchiveController');
 
-    Route::get('invoices_paid', 'InvoicesController@invoices_paid');
-    Route::get('invoices_unpaid', 'InvoicesController@invoices_unpaid');
-    Route::get('invoices_partial', 'InvoicesController@invoices_partial');
-    Route::get('invoices_export', 'InvoicesController@export');
-    Route::get('invoice_print/{id}','InvoicesController@invoice_print');
+    Route::get('/invoices_paid', 'InvoicesController@invoices_paid');
+    Route::get('/invoices_unpaid', 'InvoicesController@invoices_unpaid');
+    Route::get('/invoices_partial', 'InvoicesController@invoices_partial');
+    Route::get('/invoices_export', 'InvoicesController@export');
+    Route::get('/invoice_print/{id}','InvoicesController@invoice_print');
 
-    Route::get('section/{id}', 'InvoicesController@getproducts');
-    Route::get('edit_invoice/{id}', 'InvoicesController@edit');
-    Route::get('show_status/{id}', 'InvoicesController@show')->name('show_status');
+    Route::get('/section/{id}', 'InvoicesController@getproducts');
+    Route::get('/edit_invoice/{id}', 'InvoicesController@edit');
+    Route::get('/show_status/{id}', 'InvoicesController@show')->name('show_status');
 
-    Route::post('update_status/{id}', 'InvoicesController@update_status')->name('update_status');
+    Route::post('/update_status/{id}', 'InvoicesController@update_status')->name('update_status');
 
-    Route::get('invoices_details/{id}', 'InvoicesDetailsController@edit');
-    Route::get('view_file/{invoice_number}/{file_name}', 'InvoicesDetailsController@open_file');
-    Route::get('download_file/{invoice_number}/{file_name}', 'InvoicesDetailsController@get_file');
-    Route::post('delete_file', 'InvoicesDetailsController@destroy')->name('delete_file');
+    Route::get('/invoices_details/{id}', 'InvoicesDetailsController@edit');
+    Route::get('/view_file/{invoice_number}/{file_name}', 'InvoicesDetailsController@open_file');
+    Route::get('/download_file/{invoice_number}/{file_name}', 'InvoicesDetailsController@get_file');
+    Route::post('/delete_file', 'InvoicesDetailsController@destroy')->name('delete_file');
     //InvoiceAttachments
-    Route::resource('sections', 'SectionsController');
-    Route::resource('products', 'ProductsController');
-    Route::resource('InvoiceAttachments', 'InvoicesAttachmentsController');
-    Route::get('phone-auth', 'PhoneAuthController@index');
+    Route::resource('/sections', 'SectionsController');
+    Route::resource('/products', 'ProductsController');
+    Route::resource('/InvoiceAttachments', 'InvoicesAttachmentsController');
+    Route::get('/phone-auth', 'PhoneAuthController@index');
 
-    Route::resource('roles','RoleController');
+    Route::resource('/roles','RoleController');
 
-    Route::resource('users','UserController');
+    Route::resource('/users','UserController');
     //reports start
-    Route::get('invoices_report', 'InvoicesReportController@index');
-    Route::post('invoices_search', 'InvoicesReportController@invoices_search');
-    Route::get('customers_report', 'CustomersReporController@index')->name("customers_report");
-    Route::post('customers_search', 'CustomersReporController@customers_search');
+    Route::get('/invoices_report', 'InvoicesReportController@index');
+    Route::post('/invoices_search', 'InvoicesReportController@invoices_search');
+    Route::get('/customers_report', 'CustomersReporController@index')->name("customers_report");
+    Route::post('/customers_search', 'CustomersReporController@customers_search');
     //notifications start
-    Route::get('MarkAsRead_all','InvoicesController@MarkAsRead_all')->name('MarkAsRead_all');
-    Route::get('unreadNotifications_count', 'InvoicesController@unreadNotifications_count')->name('unreadNotifications_count');
-    Route::get('unreadNotifications', 'InvoicesController@unreadNotifications')->name('unreadNotifications');
+    Route::get('/MarkAsRead_all','InvoicesController@MarkAsRead_all')->name('MarkAsRead_all');
+    Route::get('/unreadNotifications_count', 'InvoicesController@unreadNotifications_count')->name('unreadNotifications_count');
+    Route::get('/unreadNotifications', 'InvoicesController@unreadNotifications')->name('unreadNotifications');
 });
-// Route::resource('invoices', 'InvoicesController')->middleware('auth');
-// Route::resource('sections', 'SectionsController')->middleware('auth');
-// Route::resource('products', 'ProductsController')->middleware('auth');
-Route::get('/{page}', 'AdminController@index');
+// Route::resource('/invoices', 'InvoicesController')->middleware('auth');
+// Route::resource('/sections', 'SectionsController')->middleware('auth');
+// Route::resource('/products', 'ProductsController')->middleware('auth');
+Route::get('//{page}', 'AdminController@index');
